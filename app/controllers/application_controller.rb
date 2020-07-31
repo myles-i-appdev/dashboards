a8c387264ce984c742c6b2927bb3add1495c35f6
Myles Iribarne <myles.iribarne@gmail.com>
2020-07-30 18:32:20 -0700

    finished forex converter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,30 @@
 class ApplicationController < ActionController::Base
+    def homepage
+        render({template: "homepage.html.erb"})
+    end
+
+    def forex
+        raw_file = open("https://api.exchangerate.host/symbols").read
+        @data = JSON.parse(raw_file)
+        render({template: "forex.html.erb"})
+    end
+
+    def covid
+        render({template: "covid.html.erb"})
+    end
+
+    def from_currency
+        raw_file = open("https://api.exchangerate.host/symbols").read
+        @data = JSON.parse(raw_file)
+        @from_currency = params.fetch("from_currency")
+        render({template: "from_currency.html.erb"})
+    end
+
+    def to_currency
+        @from_currency = params.fetch("from_currency")
+        @to_currency = params.fetch("to_currency")
+        raw_file = open("https://api.exchangerate.host/convert?from=" + @from_currency + "&to=" + @to_currency).read
+        @data = JSON.parse(raw_file)
+        render({template: "to_currency.html.erb"})
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get("/", {controller: "application", action: "homepage"})
+  get("/forex", {controller: "application", action: "forex"})
+  get("/forex/:from_currency", {controller: "application", action: "from_currency"})
+  get("/forex/:from_currency/:to_currency", {controller: "application", action: "to_currency"})
+  get("/covid", {controller: "application", action: "covid"})
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
